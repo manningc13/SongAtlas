3710eed45a1d4d8b9b257eef58e68518fd9decc4
Chris Clark <c.clark1928@gmail.com>
2016-04-11 16:13:57 -0400

    Create initial web views and controllers

---
+++ front-end/views/header.controller.js
@@ -0,0 +1,10 @@
+"use strict";
+(function(){
+    angular
+        .module("SongAtlas")
+        .controller("HeaderController", HeaderController);
+
+    function HeaderController($location) {
+        //TODO
+    }
+})();
