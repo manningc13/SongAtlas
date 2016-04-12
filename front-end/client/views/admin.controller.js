4125eca0d99a0b6d590f714d1c7135da79c6f2b3
Chris Clark <c.clark1928@gmail.com>
2016-04-12 12:50:12 -0400

    Add functions to admin controller and make 'edit' and 'create' modals in admin view

--- front-end/client/views/admin.controller.js
+++ front-end/client/views/admin.controller.js
@@ -5,8 +5,31 @@
         .controller("AdminController", AdminController);
 
     function AdminController(DataService) {
-        DataService.getTracks(function(response) {
-            console.log(response);
-        });
+        var vm = this;
+
+        vm.getTracks = getTracks;
+        vm.deleteTrack = deleteTrack;
+        vm.editTrack = editTrack;
+        vm.addTrack = addTrack;
+
+        function getTracks() {
+            DataService.getTracks(function (response) {
+                console.log(response);
+            });
+        }
+
+        function deleteTrack(trackId) {
+            //DataService.deleteTrack(trackId, function (response) {
+                console.log(trackId);
+            //});
+        }
+
+        function editTrack(track) {
+            console.log(track);
+        }
+
+        function addTrack(track) {
+            console.log(track);
+        }
     }
 })();
