6ad3570fdad0ff89cd433f311a5f31c14ead9b17
Christopher Manning <manningc13@gmail.com>
2016-04-12 20:35:06 -0400

    Completed Procedures for Back-End Selection

--- back-end/SongAtlasProcedures.sql
+++ back-end/SongAtlasProcedures.sql
@@ -14,7 +14,7 @@ CREATE PROCEDURE update_track_by_ID(IN i_track_id INT(11), IN i_name VARCHAR(50)
         SET t.track_name = i_name, t.duration = i_duration, t.album_id = i_album, t.genre = i_genre, t.spotify_id = i_spotify,
 			t.cloud_id = i_soundcloud, t.last_fm_id = i_lastfm, t.custom_url = i_custom
 		WHERE t.id = i_track_id;
-	END $$
+	END $$  
 DELIMITER ;
 
 
@@ -40,7 +40,7 @@ DELIMITER $$
 
 CREATE PROCEDURE add_track(IN i_title VARCHAR(50), IN i_genre VARCHAR(15), IN i_url VARCHAR(100) )
 	BEGIN
-		INSERT INTO tracks (track_name, genre, custom_url, album_id, artist_id) VALUES(i_title, i_genre, i_url, 7, 7);
+		INSERT INTO tracks (track_name, genre, custom_url, album_id, artist_id, spotify_id, cloud_id, last_fm_id) VALUES(i_title, i_genre, i_url, 7, 7, 81, 37, 6);
 	END $$
 DELIMITER ;
 
@@ -74,10 +74,12 @@ DELIMITER $$
 
 CREATE PROCEDURE get_track_urls(IN i_track_id INT(11) )
 	BEGIN
-		SELECT s.href, c.href, f.href
-		  FROM tracks t , spotify s, soundcloud c, last_fm f
-		  WHERE t.id = i_track_id
-		  GROUP BY t.id;
+		SELECT sp.href, sc.href, fm.href, x.custom_url
+		FROM (SELECT t.spotify_id, t.cloud_id, t.last_fm_id , t.custom_url
+			  FROM tracks t 
+		      WHERE t.id = i_track_id) x JOIN spotify sp ON x.spotify_id = sp.id
+										 JOIN soundcloud sc ON x.cloud_id = sc.id
+										 JOIN last_fm fm ON x.last_fm_id = fm.id;
 	END $$
 DELIMITER ;
 
@@ -88,3 +90,19 @@ CALL get_track_urls(100);
 CALL get_track_urls(44);
 
 
+-- Get Tracks by ID
+-- ------------------
+-- Query that results with the entire track 'object' for just a given track id
+DROP PROCEDURE IF EXISTS get_track_by_id;
+DELIMITER $$
+
+CREATE PROCEDURE get_track_by_id(IN i_track_id INT(11) )
+	BEGIN
+		SELECT * FROM tracks
+        WHERE id = i_track_id;
+	END $$
+DELIMITER ;
+
+-- TEST CASES
+CALL get_track_by_id(5);
+CALL get_track_by_id(57);
