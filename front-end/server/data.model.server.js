441b42d4cecff874a91ff0c752d3db5e506055c2
Chris Clark <c.clark1928@gmail.com>
2016-04-12 22:42:46 -0400

    Comment out non-mock code

--- front-end/server/data.model.server.js
+++ front-end/server/data.model.server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 module.exports = function(db) {
     var api = {
         getTracks: getTracks,
@@ -7,45 +7,51 @@ module.exports = function(db) {
     return api;
 
     function getTracks(callback) {
-        db.query('SELECT * FROM tracks', function(err, rows) {
-            if (err) {
-                console.log("Error: " + err.stack);
-            }
-            else {
-                callback(rows);
-            }
-        })
+        
+
+        // NON-MOCK CODE //
+        //db.query('SELECT * FROM tracks', function(err, rows) {
+        //    if (err) {
+        //        console.log("Error: " + err.stack);
+        //    }
+        //    else {
+        //        callback(rows);
+        //    }
+        //})
     }
 
     function updateTrackById(id, newTrack) {
-        var id = newTrack.id;
-        var name = newTrack.track_name;
-        var duration = newTrack.duration;
-        var album_id = newTrack.album_id;
-        var genre = newTrack.genre;
-        var spotify_id = newTrack.spotify_id;
-        var cloud_id = newTrack.cloud_id;
-        var last_fm_id = newTrack.last_fm_id;
-        var custom_url = newTrack.custom_url;
 
-        db.query('CALL update_track_by_ID('
-            + id + ','
-            + name + ','
-            + duration + ','
-            + album_id + ','
-            + genre + ','
-            + spotify_id + ','
-            + cloud_id + ','
-            + last_fm_id + ','
-            + custom_url + ')',
-            function(err, res) {
-                if (err) {
-                    console.log("Error: " + err.stack);
-                }
-                else {
-                    console.log(res);
-                    return res;
-                }
-            });
+
+        // NON-MOCK CODE //
+        //var id = newTrack.id;
+        //var name = newTrack.track_name;
+        //var duration = newTrack.duration;
+        //var album_id = newTrack.album_id;
+        //var genre = newTrack.genre;
+        //var spotify_id = newTrack.spotify_id;
+        //var cloud_id = newTrack.cloud_id;
+        //var last_fm_id = newTrack.last_fm_id;
+        //var custom_url = newTrack.custom_url;
+        //
+        //db.query('CALL update_track_by_ID('
+        //    + id + ','
+        //    + name + ','
+        //    + duration + ','
+        //    + album_id + ','
+        //    + genre + ','
+        //    + spotify_id + ','
+        //    + cloud_id + ','
+        //    + last_fm_id + ','
+        //    + custom_url + ')',
+        //    function(err, res) {
+        //        if (err) {
+        //            console.log("Error: " + err.stack);
+        //        }
+        //        else {
+        //            console.log(res);
+        //            return res;
+        //        }
+        //    });
     }
-}
+};
