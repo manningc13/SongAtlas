6d8f576ec1ffb6ebc52112455b785533d237ee54
Chris Clark <c.clark1928@gmail.com>
2016-04-12 22:30:04 -0400

    Try to call update_track_by_id stored procedure - FAIL

--- front-end/server/data.model.server.js
+++ front-end/server/data.model.server.js
@@ -18,7 +18,34 @@ module.exports = function(db) {
     }
 
     function updateTrackById(id, newTrack) {
-        //db.query()
-        return newTrack;
+        var id = newTrack.id;
+        var name = newTrack.track_name;
+        var duration = newTrack.duration;
+        var album_id = newTrack.album_id;
+        var genre = newTrack.genre;
+        var spotify_id = newTrack.spotify_id;
+        var cloud_id = newTrack.cloud_id;
+        var last_fm_id = newTrack.last_fm_id;
+        var custom_url = newTrack.custom_url;
+
+        db.query('CALL update_track_by_ID('
+            + id + ','
+            + name + ','
+            + duration + ','
+            + album_id + ','
+            + genre + ','
+            + spotify_id + ','
+            + cloud_id + ','
+            + last_fm_id + ','
+            + custom_url + ')',
+            function(err, res) {
+                if (err) {
+                    console.log("Error: " + err.stack);
+                }
+                else {
+                    console.log(res);
+                    return res;
+                }
+            });
     }
 }
